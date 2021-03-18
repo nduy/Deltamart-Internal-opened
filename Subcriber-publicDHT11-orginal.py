ee7c28aa024e1efae0be2c67e2863735b4a4faa8
Nguyen Duc-Duy <ducduy.nguyen@studenti.unitn.it>
2021-03-18 11:26:26 +0000

    Script to subcribe public Broker for DHT11 at home
    ORIGINAL VERSION
    Do nothing with the payload
    topic:"delta/dht11"
    hostname: "52.19.248.189"

---
+++ Subcriber-publicDHT11-orginal.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as mqtt
+
+topic= "panda/test0"
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("delta/dht11")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("52.19.248.189", 1883, 10)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
