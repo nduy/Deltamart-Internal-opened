18507f143cd675c980cd861aadd46f91b47b3361
Nguyen Duc-Duy <ducduy.nguyen@studenti.unitn.it>
2021-06-30 17:01:32 +0100

    Buyer Client - Midnight tested
    Query metadata from API
    Do subscribe,

---
+++ Buyer-Template/lib.py
@@ -0,0 +1,77 @@
+"""
+Supporting functions for  the Subscriber
+"""
+import paho.mqtt.client as mqtt
+import time
+import json
+
+rs=[]
+dst_topic="#"
+def on_connect(client, userdata, flags, rc,qos=0):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("#", qos=0)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    rs.append(str(msg.payload))
+    print("^^^len="+str(len(rs)))
+#    print(rs)
+
+def subscribenExport(dst_host="test.mosquitto.org",dst_topic="#",listen_time=3):
+    """
+    Subscribe to thde dest_host under dest_topic with qos
+    Params:
+    1. dst_host: destination hostname or IP of the destination MQTT Broker
+    2. dst_topic: MQTT topic to subscribe
+    3. qos: Quality of service
+    4.listen_time: The yime to listen to the Provider: IN MINUTE
+
+    Returns:
+    1. Nothing, message is appened to global variable rs
+
+    """
+    dst_topic=dst_topic
+    print(">>Subscribing to [{0}] under topic of \"{1}\" in {2} minutes".format(dst_host,dst_topic,listen_time));
+# The callback for when the client receives a CONNACK response from the server.
+    startTime = time.time()
+    endTime= startTime;
+    runTime = listen_time * 60 # Runtime of 3 seconds
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(dst_host, 1883, 10)
+    while True:
+        client.loop()
+        currentTime = time.time()
+        if (currentTime - startTime) > runTime:
+            endTime= time.time()
+            break
+    return (rs,startTime,endTime)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+#    client.loop_forever(timeout=0.01,max_packets=100)
+#    client.loop(timeout=0.5)
+
+if __name__ == '__main__':
+    samples,start_time,end_time=subscribenExport("test.mosquitto.org","#",listen_time=0.1)
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    print("SUMMARY===================================================")
+    print("{0} samples received".format(len(samples)))
+    json_string = json.dumps(samples)
+    print("=======================JSON STR"+(time.strftime('%Y-%m-%d %H:%M:%S.', time.localtime(start_time)))+"->"+time.strftime('%Y-%m-%d %H:%M:%S.', time.localtime(end_time)))
+    #print(json_string)
+    f = open("Outputfile.json", "w")
+    f.write("{\"data\":"+json_string+"}")
+    f.close()
---
+++ Buyer-Template/subscribe.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This is the code for Buyer client to
+1. Retrieves status of an establised DCA
+2. Subscribes to the DCA
+3. Randomly print some received samples
+4. Export to JSON File
+"""
+
+import re
+import ast
+import requests
+from termcolor import colored
+import pprint
+
+import lib
+from lib import *
+
+# Global variables
+BuyerID="DDN";  # A dummy user ID of the Buyer
+providID_pattern = re.compile('[A-Z0-9]{3}')
+
+dcaID="";  # A dummy Data Collection Activity
+dcaID_pattern = re.compile('[a-z0-9]{10}')
+
+def print_logo():
+    # Use a breakpoint in the code line below to debug your script.
+    with open('delta-logo-ascii-art.txt', 'r') as f:
+        for line in f:
+           print(colored(line.rstrip(),'green'))
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_logo();
+    print(colored( "      === DELTAMART DATA STREAM SUBSCRIBER ===",'red'))
+    BuyerID = input("Enter YOUR User ID: ");
+    # Validate User name until it is good enough
+    '''
+    while (providID_pattern.match(BuyerID)== None):
+        BuyerID = input("Incorrect Buyer ID, Reenter your UserID: ");
+
+    dcaID = input("Enter ID of the Data Collection Activity (10 characters: ");
+    '''
+    # Print out inputed data
+    print("===========================")
+    print("==You entered the following ìnormation")
+# print("UserID:{0}ta Collection ID{1}" ".format(BuyerID));
+    print("Now looking for establised Data Collection Activity for user {}".format(BuyerID));
+
+x = requests.get('http://localhost/i/dcaquery.php?BuyerID='+BuyerID);
+print("Now parse the results");
+print(x.text)
+dcaresult = ast.literal_eval(x.text);
+print('There are '+str(dcaresult['Count'])+ " DCA established for you (as a Buyer):");
+results = dcaresult['Results'];
+for i in range(0,dcaresult['Count']):
+    print(colored("((-- Index={0}--))".format(i),'blue'))
+#    print("* {0}".format(results[i]));###########################OK
+    results[i]['Provider.Email']= results[i]['Email']
+    results[i].pop('Email')
+    results[i]['Provider.Fullname'] = results[i]['Fullname']
+    results[i].pop('Fullname')
+    results[i]['Provider.IPAddress'] = results[i]['IPAddress']
+    results[i].pop('IPAddress')
+    results[i]['Provider.ZTNetworkID'] = results[i]['ZTNetworkID']
+    results[i].pop('ZTNetworkID')
+    results[i]['Provider.UserID'] = results[i]['UserID']
+    results[i].pop('UserID')
+    results[i]['Provider.Online'] = results[i]['Online']
+    results[i].pop('Online')
+    pprint.pprint(results[i])
+#    print(results[i])
+
+
+Sellecteddca_index=0
+Sellecteddca_index = int(input("Enter index of the Data Collection you want to recieve data (number from 0 to {0}:".format(dcaresult['Count']-1)));
+#print(type(Sellecteddca_index))
+#print(type(dcaresult['Count']))
+while (Sellecteddca_index<0 or Sellecteddca_index>int(dcaresult['Count'])-1):
+    Sellecteddca_index = input("[!]Incorrect input. Reenter index of the Data Collection you want to recieve data (number from 0 to {0}:".format(dcaresult['Count']-1));
+############################################
+listenTime = float(input("Enter the period of time you want to listen to Provider from now (in minutes): "));
+# Validate User name until it is good enough
+while (listenTime<=0 or listenTime>=60):
+    BuyerID = input("Incorrect Buyer ID, Reenter listening time range[0.0,59.99]:");
+# Now do subscribe LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+print(results[Sellecteddca_index])
+Pro_IP= results[Sellecteddca_index]['Provider.IPAddress']
+Pro_topic= results[Sellecteddca_index]['MQTTopic']
+
+samples,start_time,end_time = lib.subscribenExport(Pro_IP,Pro_topic,listen_time=listenTime)
+
+### Write to JSON file
+print("SUMMARY===================================================")
+print("==Tototal samples received: {0}".format(len(samples)))
+json_string = json.dumps(samples)
+print("=======================LISTENNED  TO PROVIDER. FROM Local_Time"+(time.strftime('%Y-%m-%d %H:%M:%S.', time.localtime(start_time)))+" TO Local_Time"+time.strftime('%Y-%m-%d %H:%M:%S.', time.localtime(end_time)))
+#print(json_string)
+f = open("Outputfile.json", "w")
+f.write("{\"data\":"+json_string+"}")
+f.close()
+
